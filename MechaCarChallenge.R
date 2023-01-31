679d16cee793cc3fdda28e099f93225b3a5ba089
scottemac00 <114544346+scottemac00@users.noreply.github.com>
2023-01-31 16:23:02 -0500

    thru Part 1 and in to Part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+# Part 1
 # Load the dplyr package
 library(dplyr)
 # Import and read the csv into a dataframe
@@ -6,3 +7,11 @@ mpg<- read.csv('MechaCar_mpg.csv', stringsAsFactors = F)
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mpg)
 # Use the summary function to determine p-value & r-squared
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mpg))
+
+# Part 2
+# Create a summary statistics table to show:
+# suspension coil PSI continuous variable across all lots
+# PSI mean, median, variance, and std dev
+# Import and read the csv into a dataframe
+coil <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
+# Create summary dataframe
